63b3152c1be04dc3629fad07e116188649d01ff2
Vet86 <vitalyev_aleksey@mail.ru>
2020-03-12 23:14:44 +0300

    en: Add UI app. ru: Добавлено UI приложение.

---
+++ CheckClinicUINew/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using CheckClinic.Interfaces;
+using CheckClinic.UI;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CheckClinicUINew
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainVM();
+        }
+
+        private void onNotifyButton(object sender, RoutedEventArgs e)
+        {
+            var doctor = (IDoctor)((Button)sender).DataContext;
+            ((MainVM)DataContext).AddOservable(doctor);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ((MainVM)DataContext).Save();
+        }
+    }
+}
